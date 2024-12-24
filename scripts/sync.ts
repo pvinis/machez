26fed11529d71deab84c74fe287917d6bac6ae52
Pavlos Vinieratos <pvinis@gmail.com>
2024-12-24 21:57:14 +0200

    batman

---
+++ scripts/sync.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S deno run --allow-read
+async function readApps() {
+	const content = await Deno.readTextFile('apps.txt')
+	return content
+		.split('\n')
+		.map((line) => line.trim())
+		.filter(Boolean)
+}
+
+
+const a = await readApps()
+console.log(a)
