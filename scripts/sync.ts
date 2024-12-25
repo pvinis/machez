2b35919bfc064fc315a89fab5f06c943a9fa4b93
Pavlos Vinieratos <pvinis@gmail.com>
2024-12-25 12:41:47 +0200

    ok

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env -S deno run --allow-read
 async function readApps() {
-	const content = await Deno.readTextFile('apps.txt')
+  const content = await Deno.readTextFile("apps.txt");
 	return content
-		.split('\n')
+    .split("\n")
 		.map((line) => line.trim())
-		.filter(Boolean)
+    .filter(Boolean);
+}
 }
 
 
